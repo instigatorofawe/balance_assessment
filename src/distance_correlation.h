0deea256c8d9b9b7935d820331fe2c1acfea126c
Ran Liu <xabsox@gmail.com>
2023-10-14 16:27:50 -0700

    Bugfix in armadillo import

--- src/distance_correlation.h
+++ src/distance_correlation.h
@@ -1,7 +1,10 @@
 #ifndef DISTANCE_CORRELATION_
 #define DISTANCE_CORRELATION_
 
-#include <Rcpp.h>
+// #include <Rcpp.h>
+#include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+
 using namespace Rcpp;
 
 NumericMatrix dcenter(NumericMatrix x);
